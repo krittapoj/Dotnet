4ceada0d28cf2bb67382fa8f99ff4f8ce0c11125
MeowOreo <92874536+krittapoj@users.noreply.github.com>
2025-07-07 20:17:48 +0700

    commit frist

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
-﻿using System;
-
-namespace Dotnetbegins;
-
-    class program{
-    static void Main(string[] args)
-    {
-        int n;
-        do
-        {
-            Console.Write("Loop ");
-            int.TryParse(Console.ReadLine(), out n);
-            Console.WriteLine("Entered number is: {0}", n);
-        } while (n != 0);
-        Console.WriteLine("Finished");
-    }
-    }
-
-    
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
